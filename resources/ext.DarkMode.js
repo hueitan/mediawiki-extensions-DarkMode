955e7bdf3ee4fc5897286f140163f6376c7e7cda
Huei Tan <huei90@gmail.com>
2021-05-20 17:27:14 +0200

    Promise event for updating the darkmode state

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -1,16 +1,31 @@
 $( function () {
+	const api = new mw.Api();
+	let isLoading = false;
 	var darkMode = document.documentElement.classList.contains( 'client-dark-mode' );
 
 	function updatetext() {
 		$( '.darkmode-link' ).text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
 	}
+
+	function updatingText() {
+		$( '.darkmode-link' ).text( 'Updating...' )
+		isLoading = true;
+	}
+
 	$( updatetext() );
 	$( '.darkmode-link, .darkmode-link-mobilemenu' ).on( 'click', function ( e ) {
 		e.preventDefault();
+
+		if ( isLoading ) return;
+
 		darkMode = !darkMode;
 
-		$( document.documentElement ).toggleClass( 'client-dark-mode', darkMode );
-		updatetext();
-		new mw.Api().saveOption( 'darkmode', darkMode ? 1 : 0 );
+		updatingText();
+
+		api.saveOption( 'darkmode', darkMode ? 1 : 0 ).then( () => {
+			$( document.documentElement ).toggleClass( 'client-dark-mode', darkMode );
+			updatetext();
+			isLoading = false;
+		});
 	} );
 } );
